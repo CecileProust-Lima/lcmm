1c9bf73ca4a4c448a84585d9bf98f3d1e4b76177
CecileProust-Lima <cecile.proust-lima@inserm.fr>
2021-06-17 20:56:20 +0200

    Rd files and checks

--- R/predictClass.R
+++ R/predictClass.R
@@ -1,18 +1,19 @@
 #' Posterior classification and class-membership probabilities
 #' 
-#' This function provides the posterior classification and posterior individual class-membership probabilities for external data.
+#' This function provides the posterior classification and posterior individual 
+#' class-membership probabilities for external data.
 #' 
 #' @param model an object inheriting from class \code{hlme}, \code{lcmm}, 
 #' \code{Jointlcmm} or \code{multlcmm} representing a general latent class
 #' mixed model.
-#' @param newdata data frame containing the data from which predictions are computed.
+#' @param newdata data frame containing the data from which predictions are to be computed.
 #' The data frame should include at least all the covariates listed in model$Xnames2,
 #' the outcome(s) and the grouping structure. Names should match exactly.
-#' @param subject character specifying the name of the grouping strucuture.
+#' @param subject character specifying the name of the grouping structure.
 #' If NULL (the default), the same as in the model will be used.
-#' @return a matrix with 2+ng columns : the grouping structure, the predicted class and the
-#' posterior class-membership probabilities.
-#' @author Cecile Proust-Lima, Viviane Philipps, Sasha Cuau
+#' @return a matrix with 2+ng columns: the grouping structure, the predicted class and the
+#' ng posterior class-membership probabilities.
+#' @author Sasha Cuau, Viviane Philipps, Cecile Proust-Lima
 #' @export 
 #' @examples
 #' library(NormPsy)
@@ -22,7 +23,6 @@
 #' data = paquid, ng = 2, mixture =~ age65+I(age65^2), B = c(0, 60, 40, 0, -4, 0, -10, 10,
 #' 212.869397, -216.421323,456.229910, 55.713775, -145.715516, 59.351000, 10.072221))
 #' predictClass(m2b, newdata=paquid[1:6,])
-
 
 
 predictClass <- function(model, newdata, subject=NULL){
