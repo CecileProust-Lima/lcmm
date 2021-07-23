6b64d10e9b888f4b3e5238b69577c35ac3fc86d7
VivianePhilipps <Viviane.Philipps@u-bordeaux.fr>
2021-07-23 14:10:19 +0200

    stops in predictY

--- R/predictY.multlcmm.R
+++ R/predictY.multlcmm.R
@@ -11,6 +11,14 @@ if (!inherits(newdata, "data.frame")) stop("newdata should be a data.frame objec
 #if(missing(var.time)) stop("missing argument 'var.time'")
 #if(!(var.time %in% colnames(newdata))) stop("'var.time' should be included in newdata")
 
+if(any(x$linktype==3))
+{
+     if(methInteg==0) stop("predictions for ordinal outcomes are only available with MC method. Please use methInteg=1 and set nsim argument.")
+     if(any(x$linktype!=3)) stop("predictions for mixed outcomes (ordinal and continuous) are not available yet.")
+     if(any(x$idcor>0)) stop("predictions with BM or AR correlations are not available yet.")
+     if(any(x$idcontr)>0) stop("predictions with constrasts are not available yet.")
+}
+
 if(x$conv==1 | x$conv==2 | x$conv==3) 
 {
   if(x$conv==2 & draws==TRUE)
