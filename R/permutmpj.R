1a655740bbf1d4b0b96d13ed8528d6d19b083eab
VivianePhilipps <Viviane.Philipps@u-bordeaux.fr>
2023-01-30 17:18:30 +0100

    longicall

--- R/permutmpj.R
+++ R/permutmpj.R
@@ -13,7 +13,7 @@ permutmpj <- function(m,order,estim=TRUE)
         z$verbose <- FALSE
         z[[1]] <- as.name("Jointlcmm")
         
-        mjoint <- eval(z)
+        mjoint <- eval(z, envir=parent.frame())
         mjoint_perm <- permut(mjoint, order=order, estim=FALSE)
 
         bperm <- mjoint_perm$best[1:sum(m$N[1:3])]        
@@ -25,7 +25,7 @@ permutmpj <- function(m,order,estim=TRUE)
                   fixed=formula(paste(m$Names$Yname[1], "~1")), random=~-1, mixture=~1,
                   B=c(m$best[1:m$N[1]], rep(0,m$ng),1), maxiter=0, verbose=FALSE)
         if(length(m$call$classmb)) z$classmb <- m$call$classmb
-        mclass <- eval(z)
+        mclass <- eval(z, envir=parent.frame())
 
         mclass_perm <- permut(mclass, order=order, estim=FALSE)
         bperm <- mclass_perm$best[1:m$N[1]]
@@ -33,15 +33,16 @@ permutmpj <- function(m,order,estim=TRUE)
 
     
     ## permuter les modeles longitudinaux
-    mlong <- eval(m$call$longitudinal)
+    #mlong <- eval(m$call$longitudinal, envir=parent.frame())
     avt <- sum(m$N[1:3])
     for(k in 1:m$K)
     {
-        z <- mlong[[k]]$call
+        z <- m$longicall[[k]] #mlong[[k]]$call
         z$B <- c(rep(0,m$ng-1), m$best[avt+1:m$npmK[k]])
         z$maxiter <- 0
+        z$posfix <- NULL
         z$verbose <- FALSE
-        mk <- eval(z)
+        mk <- eval(z, envir=parent.frame())
         
         mk_perm <- permut(mk, order=order, estim=FALSE)        
         bperm <- c(bperm, mk_perm$best[-c(1:(m$ng-1))])
